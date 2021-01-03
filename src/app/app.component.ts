b96c517b1b54b1cd80b2a9bf15163f28378c22e5
Yuta <>
2021-01-03 19:59:53 +0900

    表示したものを選択で詳細表示

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,6 @@
 import { Component } from '@angular/core'
 import { Task } from './task'
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
-})
-export class AppComponent {
-	title = 'todo-app';
-	task: Task = {
-		id: 1,
-		name: '田中さんにメール'
-	}
-	tasks = TASKS;
-}
-
 const TASKS: Task[] = [
   { id: 11, name: '企画ロードマップ作成' },
   { id: 12, name: '山田さんにメール返信' },
@@ -22,3 +8,24 @@ const TASKS: Task[] = [
   { id: 14, name: 'ブログ更新' },
   { id: 15, name: '新卒技術研修' }
 ];
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+export class AppComponent {
+	title = 'todo-app';
+	task: Task = {
+		id: 1,
+		name: '田中さんにメール'
+	}
+	tasks = TASKS;
+	
+	selectedTask: Task
+
+	onSelect(task: Task): void {
+		this.selectedTask = task;
+	}
+}
